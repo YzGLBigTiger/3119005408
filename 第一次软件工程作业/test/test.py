b9978ecd07e13b8fb734188c43cb6651e3acdefd
3119005408陈泽同_local <834663474@qq.com>
2021-10-09 22:30:29 +0800

    测试模块

--- 第一次软件工程作业/test/test.py
+++ 第一次软件工程作业/test/test.py
@@ -1,16 +1,17 @@
 import sys
+
 sys.path.append("..")
 import cal_sim_cosine as cal
 
 
-def test():
+def test1():
     result_file = open("test_result", "w", encoding="utf-8")
 
     result = [cal.cal_sim_cosine("测试文本\\orig.txt", "测试文本\\orig_0.8_add.txt"),
               cal.cal_sim_cosine("测试文本\\orig.txt", "测试文本\\orig_0.8_del.txt"),
               cal.cal_sim_cosine("测试文本\\orig.txt", "测试文本\\orig_0.8_dis_1.txt"),
               cal.cal_sim_cosine("测试文本\\orig.txt", "测试文本\\orig_0.8_dis_10.txt"),
-              cal.cal_sim_cosine("测试文本\\orig.txt", "测试文本\\orig_0.8_dis_15.txt")
+              cal.cal_sim_cosine("测试文本\\orig.txt", "测试文本\\orig_0.8_dis_15.txt"),
               ]
 
     result_file.write("测试结果接近，不保留两位小数\n")
@@ -20,4 +21,14 @@ def test():
     result_file.close()
 
 
-test()
+def test2():
+    result_file = open("test_result", "w", encoding="utf-8")
+
+    result = cal.cal_sim_cosine("test.py", "测试文本\\orig_0.8_del.txt")
+
+    result_file.write(str(result) + "\n")
+    result_file.close()
+
+
+test1()
+# test2()
