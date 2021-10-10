7ce5f06b92753459beafb9a38598f86847c544c8
3119005408陈泽同_local <834663474@qq.com>
2021-10-10 21:19:36 +0800

    更新单元测试内容

--- 第一次软件工程作业/test/cal_sim_cosine_test.py
+++ 第一次软件工程作业/test/cal_sim_cosine_test.py
@@ -35,9 +35,13 @@ class CalSimCosineTest(unittest.TestCase):
         self.assertEqual([1, 2, 0], cal.get_vector([0, 1, 1], [0, 1, 2]))
 
     def test_cal_vector_cos(self):
-        self.assertEqual(1, cal.cal_vector_cos([1, 1, 1], [1, 1, 1]))
+        self.assertEqual(1, round(cal.cal_vector_cos([1, 1, 1], [1, 1, 1]), 2))
         self.assertEqual(0, cal.cal_vector_cos([0, 1, 9], [3, 0, 0]))
 
-    def test_cal_sim_cossine(self):
+    def test_cal_sim_cosine(self):
         self.assertEqual(1.0, cal.cal_sim_cosine("测试文本//orig.txt", "测试文本//orig.txt"))
         self.assertGreater(1.0, cal.cal_sim_cosine("测试文本//orig.txt", "测试文本//orig_0.8_add.txt"))
+
+
+if __name__ == "__main__":
+    unittest.main()
